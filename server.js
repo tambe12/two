ceed55721da6068b2b88dafd9a221ad2c91030ad
tambe12 <clavecris16@gmail.com>
2025-05-09 00:19:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,3 +12,10 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+/*
+npm init -y
+npm i express
+npm i <je error asel te>
+mode server.js
+*/
